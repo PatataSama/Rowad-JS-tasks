4623fc3a45702a898f2a518887e64edb05b3360e
PotaSama <141175077+PotaSama@users.noreply.github.com>
2024-07-22 14:12:18 +0300

    Add files via upload

---
+++ task-2/mani.js
@@ -0,0 +1,16 @@
+let btn1 = document.getElementById("btn-1")
+let btn2 = document.getElementById("btn-2")
+let body = document.getElementsByName("body")
+
+let color2 = btn1.value
+
+let root = document.querySelector(":root")
+
+btn1.addEventListener("change" , function () {
+    let color1 = btn1.value
+    root.style.setProperty("--color-1", color1)
+})
+btn2.addEventListener("change" , function () {
+    let color2 = btn2.value
+    root.style.setProperty("--color-2", color1)
+})
